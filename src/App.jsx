3703802c77340e234a21a38787b4166b080b564c
unknown <unknown>
2025-05-29 12:44:36 +0700

    remove router

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,8 @@ import { useEffect, useState } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { useAccount } from 'wagmi';
 
-import Landing from './pages/Landing/Loadable.jsx';
-import MyStake from './pages/MyStake/Loadable.jsx';
-import Ref from './pages/Ref/Loadable.jsx';
-import Rewards from './pages/Rewards/Loadable.jsx';
-import Stake from './pages/Stake/Loadable.jsx';
-import Swap from './pages/Swap/Loadable.jsx';
-import Transactions from './pages/Transactions/Loadable.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Landing />
-  },
-  {
-    path: '/farm',
-    element: <Stake />
-  },
-  {
-    path: '/swap',
-    element: <Swap />
-  },
-  {
-    path: '/referral',
-    element: <Ref />
-  },
-  {
-    path: '/my-farm',
-    element: <MyStake />
-  },
-  {
-    path: '/my-transactions',
-    element: <Transactions />
-  },
-  {
-    path: '/referral-reward',
-    element: <Rewards />
-  }
 ]);
 
 const App = () => {
